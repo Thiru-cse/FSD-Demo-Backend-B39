5124af27787331706167cff9264d9539f7fe1a59
Thiru <69345132+Thiru-cse@users.noreply.github.com>
2026-03-04 18:51:14 +0530

    Update Stripe success and cancel URLs

--- Controllers/paymentController.js
+++ Controllers/paymentController.js
@@ -29,8 +29,8 @@ export const createCheckoutSession = async (req, res) => {
       payment_method_types: ["card"],
       line_items,
       mode: "payment",
-      success_url: "http://localhost:5173/success",
-      cancel_url: "http://localhost:5173/cancel",
+      success_url: "https://fsd-demo-frontend-b39-dm4o8ydsr-thirucses-projects.vercel.app/success",
+      cancel_url: "https://fsd-demo-frontend-b39-dm4o8ydsr-thirucses-projects.vercel.app/cancel",
     });
 
     res.json({ url: session.url });
